4f9c9446e6cffa0419e385e2fb576c6a49a2f0f7
PranayDhanke <pranaydhanke33@gmail.com>
2026-01-13 17:25:59 +0530

    deployment changes

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://gig-flow-pfch.onrender.com/", {
   withCredentials: true,
   autoConnect: false,
 });
